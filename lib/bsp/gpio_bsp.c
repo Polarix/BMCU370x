ce43af27b54ee984c7e60c18dc4f5e1b157f8255
Polarix <xuyulin91@163.com>
2025-04-16 14:04:28 +0800

    增加检测开关GPIO初始化和配置接口

--- lib/bsp/gpio_bsp.c
+++ lib/bsp/gpio_bsp.c
@@ -2,8 +2,6 @@
 //= Include files.                                          =//
 //===========================================================//
 #include <gpio_bsp.h>
-#include <ch32v20x_usart.h>
-#include <ch32v20x_dma.h>
 #include <ch32v20x_gpio.h>
 
 //===========================================================//
@@ -15,6 +13,21 @@
 //===========================================================//
 static GPIO_InitTypeDef s_gpio_input_init_struct;
 static GPIO_InitTypeDef s_gpio_output_init_struct;
+static struct
+{
+    GPIO_TypeDef*   port;
+    uint16_t        pin;
+}s_gpio_config[GPIO_ID_MAX] = 
+{
+    {GPIOB, GPIO_Pin_12}, // GPIO_ID_PULL_CH1
+    {GPIOB, GPIO_Pin_13}, // GPIO_ID_PULL_CH2
+    {GPIOB, GPIO_Pin_14}, // GPIO_ID_PULL_CH3
+    {GPIOB, GPIO_Pin_15}, // GPIO_ID_PULL_CH4
+    {GPIOC, GPIO_Pin_13}, // GPIO_ID_ONLINE_CH1
+    {GPIOC, GPIO_Pin_14}, // GPIO_ID_ONLINE_CH2
+    {GPIOC, GPIO_Pin_15}, // GPIO_ID_ONLINE_CH3
+    {GPIOD, GPIO_Pin_0},  // GPIO_ID_ONLINE_CH4
+};
 
 //===========================================================//
 //= Function definition.                                    =//
@@ -30,9 +43,26 @@ void gpio_bsp_init(void)
     
     s_gpio_input_init_struct.GPIO_Pin = GPIO_Pin_10;
     s_gpio_input_init_struct.GPIO_Speed = GPIO_Speed_50MHz;
-    s_gpio_input_init_struct.GPIO_Mode = GPIO_Mode_AF_PP;
-    GPIO_Init(GPIOB, &s_gpio_input_init_struct);
+    s_gpio_input_init_struct.GPIO_Mode = GPIO_Mode_IPU;
+    for(int gpio_idx=0; gpio_idx<GPIO_ID_MAX; ++gpio_idx)
+    {
+        s_gpio_input_init_struct.GPIO_Pin = s_gpio_config[gpio_idx].pin;
+        GPIO_Init(s_gpio_config[gpio_idx].port, &s_gpio_input_init_struct);
+    }
 }
 
-bool gpio_bsp_set_dir(void);
-void gpio_bsp_get(const void* data, uint32_t len);
+
+/* 读取GPIO的输入状态 */
+uint8_t gpio_bsp_read_gpio(gpio_id_t gpio_id)
+{
+    uint8_t val;
+    if(gpio_id < GPIO_ID_MAX)
+    {
+        val = GPIO_ReadInputDataBit(s_gpio_config[gpio_id].port, s_gpio_config[gpio_id].pin);
+    }
+    else
+    {
+        val = 1;
+    }
+    return val;
+}
--- lib/bsp/gpio_bsp.h
+++ lib/bsp/gpio_bsp.h
@@ -14,7 +14,18 @@
 //===========================================================//
 //= Data type definition.                                   =//
 //===========================================================//
-
+typedef enum _e_gpio_id_
+{
+    GPIO_ID_PULL_CH1 = 0,
+    GPIO_ID_PULL_CH2,
+    GPIO_ID_PULL_CH3,
+    GPIO_ID_PULL_CH4,
+    GPIO_ID_ONLINE_CH1,
+    GPIO_ID_ONLINE_CH2,
+    GPIO_ID_ONLINE_CH3,
+    GPIO_ID_ONLINE_CH4,
+    GPIO_ID_MAX
+}gpio_id_t;
 
 //===========================================================//
 //= Public function declaration.                            =//
@@ -25,8 +36,7 @@ extern "C"
 #endif
 
 void gpio_bsp_init(void);
-bool gpio_bsp_set_dir(void);
-void gpio_bsp_get(const void* data, uint32_t len);
+uint8_t gpio_bsp_read_gpio(gpio_id_t gpio_id);
 
 #ifdef __cplusplus
 }
